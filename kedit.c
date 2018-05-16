13f95bb4803e1f6b808856b728d1599aef130635
Kevin Schaefer <kevin@meet-app.com>
2018-05-16 13:12:04 +0200

    Resolving whitepace issue with IDE

--- kedit.c
+++ kedit.c
@@ -7,13 +7,13 @@
 struct termios orig_termios;
 
 void die(const char *s) {
-	perror(s);
-	exit(1);
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode() {
-	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-		die("tcsetattr");
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
@@ -21,12 +21,12 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_cflag |= (CS8);
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-	raw.c_cc[VMIN] = 0;
-	raw.c_cc[VTIME] = 1;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
@@ -34,18 +34,18 @@ void enableRawMode() {
 int main() {
     enableRawMode();
 
-	while(1) {
-		char c = '\0';
+    while(1) {
+        char c = '\0';
 
-		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
 
-		if (c == 'q') break;
-	}
+        if (c == 'q') break;
+    }
     return 0;
 }
